7b772ae7e7cdf1d29cb497e98a1ba7da3425b168
BabGee <jeremiahgeorge68@gmail.com>
2021-04-29 00:19:29 +0300

    added postgres to docker

--- interintel/interintel/settings.py
+++ interintel/interintel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x&hlfols6$e^p7887s)fljrc7%=mj^l4gg+!d3gk+u=dpqzbdj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -80,10 +80,15 @@ WSGI_APPLICATION = 'interintel.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'search_db',
+        'USER': 'intel',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
